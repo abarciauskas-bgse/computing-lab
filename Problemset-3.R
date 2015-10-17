aa3692eab446693bca2b64b8a8b50c380f34e8cb
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-10-17 14:46:52 +0200

    fix wald.test

--- Problemset-3.R
+++ Problemset-3.R
@@ -13,26 +13,26 @@
 #
 # D = 0
 # H0: mx - my = 0
-# H1: mx < my (or mx - my < 0)
+# H1: mx - my < 0 (e.g. mx < my)
 #
 wald.test <- function(y, x, alpha) {
   my <- mean(y)
   mx <- mean(x)
 
   # stat: wald test statistic
-  stat <- (my - mx) / sqrt((var(x)/length(x)) + (var(y)/length(y)))
+  stat <- (mx - my - 0) / sqrt((var(x)/length(x)) + (var(y)/length(y)))
   # pval of stat
   # This would be qnorm(1 - stat/2) if we were interested in a 2-tailed test
   pval <- dnorm(stat)
   # pval of alpha (e.g. size)
   alpha_stat <- qnorm(1 - alpha)
   # Reject H0 if stat > alpha_pval
-  reject <- stat > alpha_stat
+  reject <- stat < -alpha_stat
   list(stat, pval, reject)
 }
 
 # # Test
-#
+
 # alpha <- 0.05
 # ntests <- 1000
 # sample_sizes <- seq(5,300,5)
@@ -152,16 +152,16 @@ mc.test.size <- function(test, alpha, S, N) {
   test_sizes
 }
 
-# N <- matrix(c(4,5,6,7,8,9,10,11,20,21,500,520), 6, 2)
-# (wt.size <- mc.test.size(wald.test, 0.1, 20000, N))
-# (mwt.size <- mc.test.size(mann.whitney.test, 0.1, 20000, N))
+N <- matrix(c(4,5,6,7,8,9,10,11,20,21,500,520), 6, 2)
+(wt.size <- mc.test.size(wald.test, 0.1, 20000, N))
+(mwt.size <- mc.test.size(mann.whitney.test, 0.1, 20000, N))
 
-# # FIXME: This should be ylim=c(0,0.20) so something is wrong
-# plot(wt.size, t='l', col='darkred', lwd=2, ylim=c(0,0.30))
-# grid()
-# lines(mwt.size, t='l', col='darkblue', lwd=2 )
-# lines(rep(0.1, nrow(N)), col='black', lwd=1 )
-# legend("topright", c('wald','mann-whitney'), col=c('darkred','darkblue'), lty=1)
+# FIXME: This should be ylim=c(0,0.20) so something is wrong
+plot(wt.size, t='l', col='darkred', lwd=2, ylim=c(0,0.30))
+grid()
+lines(mwt.size, t='l', col='darkblue', lwd=2 )
+lines(rep(0.1, nrow(N)), col='black', lwd=1 )
+legend("topright", c('wald','mann-whitney'), col=c('darkred','darkblue'), lty=1)
 
 # `mc.test.power(test, alpha, S, N, delta)`
 #
