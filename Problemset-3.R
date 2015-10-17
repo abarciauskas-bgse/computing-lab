63a574b166280ef283ec1f3bc3dd8187c374d5e1
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-10-17 11:39:39 +0200

    Add size and power tests

--- Problemset-3.R
+++ Problemset-3.R
@@ -110,5 +110,98 @@ mann.whitney.test <- function(y, x, alpha) {
 
 # plot(sample_sizes,colMeans(reject*100),t='b',ylim=c(0,10),lwd=2,col='darkblue',ylab='Test Size',xlab='Sample Size')
 
+# `mc.test.size(test, alpha, S, N)`
+#
+# The function requires the arguments (in this order):
+#   1) `test`: the function performing the test;
+#   2) `alpha`: the significance level of the test;
+#   3) `S`: the number of simulations;
+#   4) `N`: an m × 2 matrix with elements ni1 and ni2 denoting the sample sizes of the first and second sample.
+#
+# For each row of N, the function computes the size of the test by performing
+# a monte carlo experiment. In each replication of the monte carlo experiment
+# the function simulates population one and two from a t distribution with 2.1
+# degrees of freedom. The sizes of the populations are ni1 and ni2.
+#
+# The function returns a vector of size m containg the monte carlo estimate of the test size.
+# (The i–th element of the vector contains the size corresponding to row i of the N matrix)
+#
+# REVIEW: WHERE DO WE NEED S?
+mc.test.size <- function(test, alpha, S, N) {
+  m <- nrow(N)
+  ni1 <- N[,1]
+  ni2 <- N[,2]
+  test_sizes <- rep(0, m)
 
+  for (row in 1:m) {
+    cat('.')
+    test_sizes_for_row <- rep(0, S)
+    for (sim in 1:S) {
+      first_sample_size <- ni1[row]
+      second_sample_size <- ni2[row]
+      # REVIEW: SHOULD THIS BE 1 + ...?
+      first_sample <- rt(first_sample_size, 2.1)
+      second_sample <- rt(second_sample_size, 2.1)
+      test_results <- test(first_sample, second_sample, alpha)
+      test_sizes_for_row[sim] <- test_results[2][[1]]
+    }
+    test_sizes[row] <- mean(test_sizes_for_row)
+    cat('\n')
+  }
 
+  test_sizes
+}
+
+# N <- matrix(c(4,5,6,7,8,9,10,11,20,21,500,520), 6, 2)
+# (wt.size <- mc.test.size(wald.test, 0.1, 20000, N))
+# (mwt.size <- mc.test.size(mann.whitney.test, 0.1, 20000, N))
+
+# # FIXME: This should be ylim=c(0,0.20) so something is wrong
+# plot(wt.size, t='l', col='darkred', lwd=2, ylim=c(0,0.30))
+# grid()
+# lines(mwt.size, t='l', col='darkblue', lwd=2 )
+# lines(rep(0.1, nrow(N)), col='black', lwd=1 )
+# legend("topright", c('wald','mann-whitney'), col=c('darkred','darkblue'), lty=1)
+
+# `mc.test.power(test, alpha, S, N, delta)`
+#
+# The function requires the arguments (in this order):
+#   1) `test`: the function performing the test; 
+#   2) `alpha`: the significance level of the test;
+#   3) `S`: the number of simulations;
+#   4) `N`: a vector of size two containing the sample sizes of the first and second sample;
+#   5) `delta`: a vector containing a sequence of mean values for the first sample.
+#
+# For each element of delta, the function computes the power of the test by
+#   performing a monte carlo experiment. In each replication of the monte carlo
+#   experiment the function simulates population one and two from a t
+#   distribution with 2.1 degrees of freedom. Sample 1 has mean of delta[i] and a size
+#   of N1 while sample 2 has mean zero and a size of N2.
+#
+# The function returns a vector of size m containg the monte carlo estimate of
+#   the power of the test. (The i–th element of the vector contains the power
+#   corresponding to i–th element of delta)
+#
+mc.test.power <- function(test, alpha, S, N, delta) {
+  first_sample_size <- N[1]
+  second_sample_size <- N[2]
+  powers <- rep(0, length(delta))
+
+  for (first_sample_mean_index in 1:length(delta)) {
+    first_sample <- delta[first_sample_mean_index] + rt(first_sample_size, 2.1)
+    second_sample <- 0 + rt(second_sample_size, 2.1)
+    test_results <- test(first_sample, second_sample, alpha)
+    powers[first_sample_mean_index] <- test_results[2][[1]]
+  }
+
+  powers
+}
+
+delta <- seq(0.0,5,0.05)
+(wt.power <- mc.test.power(wald.test, 0.10, 20000, c(10,13), delta))
+(mwt.power <- mc.test.power(mann.whitney.test, 0.10, 20000 , c(10,13), delta))
+plot(delta, wt.power, t='l', col='darkred', lwd=2 , ylim=c(0,1))
+grid()
+lines(delta, mwt.power, t='l', col='darkblue', lwd=2)
+
+legend("bottomright", c('wald','mann-whitney') , col=c('darkred','darkblue'), lty=1)
