1258cf7ab2966503ec05b7f5934d071c90ccebd1
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-11-15 20:06:36 +0100

    Nearly there

--- problemsets/Problemset-5.R
+++ problemsets/Problemset-5.R
@@ -41,30 +41,27 @@ ridge.reg <- function(y, X, lambda) {
 # (Compute the lasso estimator using the shooting algorithm described in the
 # (handout.)
 #
-soft.threshold <- function(x,lambda){
-  sign(x)*max( c( abs(x) - lambda , 0 ) )
-}
-
 lasso.reg <- function(y, X, lambda) {
-  iterations <- 10
-  # Number of explanatory variables
+  max.iter <- 100
   P <- ncol(X)
 
-  beta <- solve(t(X)%*%X,t(X)%*%y)
+  beta <- solve(t(X)%*%X, t(X)%*%y)
   beta.prev <- beta
 
   # Do until convergence
-  for (i in 1:iterations) {
-    for (p in 1:P)
-      # For evey coefficient, define y_i(-k) = y_i - sum(beta[,setdiff(beta, i)]%*%)
-      y.k <- y[i] - X[,setdiff(1:P,p)] %*% beta[setdiff(1:P,p)]
-      x.k <- X[,p]
-      cov <- sum(y.k * x.k)
+  for (iter in 1:max.iter) {
+    for (k in 1:P)
+      # c
+      y.minus_k <- y - X[,setdiff(1:P,k)] %*% beta[setdiff(1:P,k)]
+      x.k <- X[,k]
+      cov <- sum(y.minus_k * x.k)
       var <- sum(x.k * x.k)
 
-      # cov/var is just the least squares kth coefficient
-      beta[i] <- soft.threshold(cov/var , lambda/(2*var))
-      if (sum((beta - beta.prev)**2) < 1e-6 ) { return(beta) }
+      beta.k.ls <- cov/var
+      beta[k] <- sign(beta.k.ls)*max( c( abs(beta.k.ls) - lambda/(2*var) , 0 ) )
+      # cov/var ~ least squares kth coefficient
+      # beta[i] <- soft.threshold(cov/var , lambda/(2*var))
+      if (sum((beta - beta.prev)**2) < 1e-6) { return(beta) }
 
       beta.prev <- beta
   }
@@ -89,11 +86,33 @@ lasso.reg <- function(y, X, lambda) {
 #  4) the penalized estimation routine pen.reg (which can either be lasso.reg or ridge.reg)
 #
 # The function returns the cross.validation RSS
-#
+# 1:400,        #pointer is nobs - test_size (train_size)
+# 1:300,400:500 #pointer is train_size - test_size
+# 1:200,300:500 # pointer is train_size - 2*test_size
+# 1:100,200:500
+# ,400:500
 cross.validation <- function(y, X, lambda, pen.reg) {
-  y_subsamples <- split(y, ceiling(seq_along(y)/5))
-
-  # Estimate beta from 4 subsamples
-  beta.pen <- pen.reg(y, X, lambda)
+  nsubsamples <- 5
+  nobs <- length(y)
+  test_size <- floor(nobs/nsubsamples)
+  list_of_rss <- c()
   
+  # FIXME this offsetting is off
+  for (i in 1:nsubsamples) {
+    pointer <- nobs-i*test_size
+    if (pointer == 0) pointer <- 1
+   
+    y_train <- c(y[1:(pointer-1)], y[(test_size + pointer):nobs])
+    # although unused
+    X_train <- rbind(X[1:(pointer-1),], X[(test_size + pointer):nobs,])
+
+    y_test <- y[pointer:(test_size + pointer - 1)]
+    X_test <- X[pointer:(test_size + pointer - 1),]
+
+    # Estimate beta from 4 subsamples
+    beta.pen <- pen.reg(y_train, X_train, lambda)
+    fitted <- X_test %*% beta.pen
+    list_of_rss <- append(list_of_rss, sum((y_test - fitted)^2))
+  }
+  mean(list_of_rss)
 }
